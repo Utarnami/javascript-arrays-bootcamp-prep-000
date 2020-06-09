ce466f04be4a00e8573d2a22122fca5daced8d45
Pavel Dziamentsyeu <paveldz@mail.com>
2020-06-09 23:15:05 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -28,6 +28,5 @@ function destructivelyRemoveElementFromBeginningOfArray(chocolateBars){
 }
 
 function removeElementFromBeginningOfArray(chocolateBars){
-  chocolateBars.slice(1)
-  return chocolateBars;
+  return chocolateBars.slice(1);
 }
