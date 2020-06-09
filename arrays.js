a93bf5a6b1f6adcaeb94311fb9a7ade2ac6b5673
Pavel Dziamentsyeu <paveldz@mail.com>
2020-06-09 23:10:44 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -23,5 +23,5 @@ function accessElementInArray(array, index){
 }
 
 function destructivelyRemoveElementFromBeginningOfArray(chocolateBars.shift){
-  return chocolateBars.shift();
+  return chocolateBars();
 }
